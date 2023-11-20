032e5f7dbd9468d10f46da963a676786c88c607b
Tunde Ogunjimi <tundeogunjimi@gmail.com>
2023-11-20 16:56:41 +0100

    Added scheduled meetings page

--- src/app/meeting/meeting-routing.module.ts
+++ src/app/meeting/meeting-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {TenantMeetingsComponent} from "./tenant-meetings/tenant-meetings.component";
 import {MeetingComponent} from "./meeting/meeting.component";
+import { ScheduledMeetingsComponent } from './scheduled-meetings/scheduled-meetings.component';
 
 const routes: Routes = [
   { path: '', component: MeetingComponent},
+  { path: 'scheduled', component: ScheduledMeetingsComponent},
   { path: ':username', component: TenantMeetingsComponent},
 ];
 
